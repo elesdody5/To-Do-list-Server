3926a78307fcbe7004f0778726337f37ac36c2e1
codingcity4747 <codingcity4747@gmail.com>
2020-01-19 21:11:57 +0200

    merge server

--- To-Do-list-Server/src/serverDatabase/DataBase.java
+++ To-Do-list-Server/src/serverDatabase/DataBase.java
@@ -18,9 +18,9 @@ public class DataBase {
 
 
    
-    private static final String DATABASE_URL = "jdbc:sqlite:H:\\Projects\\ITI\\java\\project\\TODO.db";
+    //private static final String DATABASE_URL = "jdbc:sqlite:H:\\Projects\\ITI\\java\\project\\TODO.db";
     //private static final String DATABASE_URL ="jdbc:sqlite:D:\\ITI\\javaProject\\dataBase\\TODO.db";
-      //private static final String DATABASE_URL = "jdbc:sqlite:D:\\java work space\\other projects\\Java Project\\TODO.db";    
+      private static final String DATABASE_URL = "jdbc:sqlite:D:\\java work space\\other projects\\Java Project\\TODO.db";    
      // private static final String DATABASE_URL = "jdbc:sqlite:F:\\ITI\\Java\\ToDoList Project\\TODO.db";
 
    
